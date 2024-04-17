b2f365eb3a0570cff551e5eddd6184fd6f6e0323
YaoTengqi <425045233@qq.com>
2024-04-17 17:22:03 +0800

    proj1 MaxArrayDeque done, add MaxArrayDequeTest

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,40 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        this.comparator = c;
+    }
+
+    public T max() {
+        T max = items[0];
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (this.items[i] != null && comparator.compare(this.items[i], max) > 0) {
+                    max = this.items[i];
+                }
+            }
+        }
+        return max;
+    }
+
+    public T max(Comparator c) {
+        T max = items[0];
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (this.items[i] != null && comparator.compare(this.items[i], max) > 0) {
+                    max = this.items[i];
+                }
+            }
+        }
+        return max;
+    }
+}
---
+++ proj1/deque/MaxArrayDequeTest.java
@@ -0,0 +1,74 @@
+package deque;
+
+import jh61b.junit.In;
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaxArrayDequeTest {
+    Comparator<Integer> c = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer integer, Integer t1) {
+            if (integer > t1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
+
+    @Test
+    public void createMADeque() {
+
+        MaxArrayDeque<Integer> mad = new MaxArrayDeque<Integer>(c);
+        mad.addFirst(1);
+        mad.addLast(2);
+        mad.addLast(3);
+        assertEquals(3, mad.size());
+    }
+
+    @Test
+    public void testMax() {
+        MaxArrayDeque<Integer> mad = new MaxArrayDeque<Integer>(c);
+        mad.addFirst(1);
+        mad.addLast(2);
+        mad.addLast(3);
+        Integer max = mad.max();
+        assertEquals(new Integer(3), max);
+    }
+
+    @Test
+    public void testMaxComparator() {
+        MaxArrayDeque<Integer> mad = new MaxArrayDeque<Integer>(c);
+        mad.addFirst(1);
+        mad.addLast(2);
+        mad.addLast(3);
+        mad.addLast(3);
+        mad.addLast(3);
+        mad.addLast(100);
+        Integer max = mad.max(c);
+        assertEquals(new Integer(100), max);
+    }
+
+    @Test
+    public void randomMaxTest() {
+        MaxArrayDeque<Integer> mad = new MaxArrayDeque<Integer>(c);
+        Random random = new Random();
+        int max = -128;
+        for (int i = 0; i < 100; i++) {
+            int num = random.nextInt();
+            mad.addLast(num);
+            if (max < num) {
+                max = num;
+            }
+        }
+        int my_max = mad.max(c);
+        System.out.println("Right Max: " + max);
+        System.out.println("My Max: " + my_max);
+        assertEquals(my_max, max);
+    }
+}
